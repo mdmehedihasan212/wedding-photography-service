467759b1ab91d31a3034a2baef08f97d6fb899ca
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-04-16 22:54:59 +0600

    install react router npm and setup cdn link bootstrap in index.html and set routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home/Home/Home';
+import About from './Pages/About/About';
+import Header from './Pages/Shared/Header/Header';
+import Footer from './Pages/Shared/Footer/Footer';
 
 function App() {
   return (
-    <div>
-      <h1>Wedding Photography</h1>
+    <div className="App">
+      <h1>Welcome to React Router!</h1>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }

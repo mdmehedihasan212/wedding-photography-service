5caf63920d40a5bab073d9d09ae56484b56feff2
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-04-16 23:35:56 +0600

    add header navbar with name and link

--- src/App.js
+++ src/App.js
@@ -4,15 +4,21 @@ import Home from './Pages/Home/Home/Home';
 import About from './Pages/About/About';
 import Header from './Pages/Shared/Header/Header';
 import Footer from './Pages/Shared/Footer/Footer';
+import Services from './Pages/Home/Services/Services';
+import Blogs from './Pages/Blogs/Blogs';
+import Login from './Pages/Login/Login';
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to React Router!</h1>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="about" element={<About />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
       <Footer />
     </div>
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div>
-            <h1>Header</h1>
-        </div>
+        <>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+                <div className="container-fluid">
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse container" id="navbarTogglerDemo01">
+                        <Link className="navbar-brand" to="/">Wedding Photography</Link>
+                        <ul className="navbar-nav mb-2 mb-lg-0 ms-auto">
+                            <li className="nav-item">
+                                <Link className="nav-link active ms-3" to="/">Home</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link active ms-3" to="/services">Services</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link active ms-3" to="/blogs">Blogs</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link active ms-3" to="/about">About</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link active ms-3" to="/login">Login</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </>
     );
 };
 

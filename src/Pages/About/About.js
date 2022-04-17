088afee82ee002c1d1b2aef6b4783661ee5e91ab
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-04-17 23:57:42 +0600

    add about page and set photo and my goal

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import image from '../../Assets/My image/Photo Of Mehedi Hasan.jpg';
 
 const About = () => {
     return (
-        <div>
-            <h1>About Me</h1>
-        </div>
+        <section>
+            <div>
+                <div className="card mb-3 mx-auto mt-4" style={{ width: "540px" }}>
+                    <div className="row g-0">
+                        <div className="col-md-4">
+                            <img src={image} className="img-fluid rounded-start p-2" alt="..." />
+                        </div>
+                        <div className="col-md-8">
+                            <div className="card-body">
+                                <h5 className="card-title">Md Mehedi Hasan</h5>
+                                <ul className="card-text">
+                                    <li>I want to learn web development and get a job as a web developer in a reputed company in the world.</li>
+                                    <li>I want to reach my dream goal by working hard in the coming days to fulfill this dream of mine.</li>
+                                    <li>Pray to Allah for me.</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section >
     );
 };
 
--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Blogs = () => {
     return (
-        <section className="d-flex m-4 p-2 text-break">
+        <section className="d-flex m-4 p-2">
             <div>
                 <h3>(1) Difference between authorization and authentication ?</h3>
                 <ul>
